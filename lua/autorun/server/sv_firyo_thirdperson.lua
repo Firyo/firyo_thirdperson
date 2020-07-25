b8c9126a33d68539fc723fe411c6cc175d408c52
Firyo <56397435+Firyo@users.noreply.github.com>
2020-07-25 13:02:01 +0200

    Update sv_firyo_thirdperson.lua

--- lua/autorun/server/sv_firyo_thirdperson.lua
+++ lua/autorun/server/sv_firyo_thirdperson.lua
@@ -2,6 +2,5 @@ util.AddNetworkString("Firyo:ThirdPerson:Toggle")
 
 hook.Add("ShowHelp", "Firyo:ThirdPerson:F1", function(ply)
     net.Start("Firyo:ThirdPerson:Toggle")
-    net.WriteInt(1, 4)
     net.Send(ply)
 end)
